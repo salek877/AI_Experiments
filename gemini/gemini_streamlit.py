ac3385844920935b3fb75675234f75047eabff5b
Abu Salek <64322352+salek877@users.noreply.github.com>
2023-12-21 10:18:17 +0600

    Update gemini_streamlit.py
    fix error

--- gemini/gemini_streamlit.py
+++ gemini/gemini_streamlit.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import textwrap
 import google.generativeai as genai
-from IPython.display import display
 from IPython.display import Markdown
 import PIL.Image
 
-def load_img(file_pth):
-    return PIL.Image.open(file_pth)
+
 
 def to_markdown(text):
     text = text.replace('•', '  *')
@@ -22,40 +20,58 @@ def vision(img):
     vision_output = response.text
     return vision_output
 
+
+        
 def chat(inpt_frm_vsn):
     model = genai.GenerativeModel('gemini-pro')
     chat = model.start_chat(history=[])
-    chat
 
-    res = inpt_frm_vsn + 'Tell me the fruit name and nutrition'
+    message_count = 0
 
-    while True:
-        response = chat.send_message(res, generation_config=genai.types.GenerationConfig(
-            max_output_tokens=200
+    continue_chat = True
+
+    while continue_chat:
+        if message_count == 0:
+            input_msg = inpt_frm_vsn + ' Tell me the fruit name and nutrition. Complete answer in 55 words. Never show the word count.'
+        else:
+            input_key = f"chat_input_{message_count}"
+            user_input = st.chat_input('Please write your message here...', key=input_key)
+            if user_input:
+                input_msg = user_input + ' Complete answer in 55 words. Never show the word count.'
+            else:
+                continue_chat = False
+                break
+
+        response = chat.send_message(input_msg, generation_config=genai.types.GenerationConfig(
+            max_output_tokens=100
         ))
         to_markdown(response.text)
-
         st.text('Gemini: ' + response.text)
-        res = st.text_input('User: ')
+        message_count += 1
+
+        
 
 def main():
     st.title("Gemini")
 
-    img_path = 'assets/apple.jpg'
-    img = load_img(img_path)
-    st.image(img, caption='Uploaded Image.', use_column_width=True)
 
-    GOOGLE_API_KEY = 'AIzaSyA1bYLSWmPCXiN7C6LdyrLtXmtw7ylbuS0'
+    GOOGLE_API_KEY = 'AIzaSyDMDS5TghIYsEV4nD6FsC8PDCtqGvkuUls'
 
     genai.configure(api_key=GOOGLE_API_KEY)
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-    vision_result = vision(img)
-    st.text(vision_result)
+    if uploaded_file is not None:
+        img = PIL.Image.open(uploaded_file)
+        st.image(img, caption='Uploaded Image.', use_column_width=True)
 
-    check_point = st.text_input("If you want to continue chat write 'nutrition': ")
+        vision_result = vision(img)
+        st.text(vision_result)
 
-    if check_point.lower() == 'nutrition':
-        chat(vision_result)
+        check_point = st.text_input("If you want to continue chat write 'nutrition': ")
+
+        if check_point.lower() == 'nutrition':
+            chat(vision_result)
+
 
 if __name__ == "__main__":
     main()
